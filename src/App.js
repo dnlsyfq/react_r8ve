819c998c405f6942a17b9157af19a21d71605ba7
= <dan90@live.com.my>
2020-09-06 02:18:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// component 1
+
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+// component 2    
+
+        // <button type="button" class="btn btn-primary">
+        //   My Button
+        // </button>
+
+
+// component 3
+
+    // <section>
+    //   <header>
+    //     <h1>A Header</h1>
+    //   </header>
+    //   <nav>
+    //     <a href="#">Nav Item</a>
+    //   </nav>
+    //     <main>
+    //       <p>The main content...</p>
+    //     </main>
+    //   <footer>
+    //     <small>&copy; 2016</small>
+    //   </footer>
+    // </section>
+
+
+
+//   );
+// }
+
+// export default App;
+
+
+// template
+
+// function App() {
+//   return ();
+// }
+
+// export default App;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import { render } from '@testing-library/react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// component 1
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// class 
+
+// class MyComponent extends Component {
+//   render(){
+//     return (
+//       <section>
+//         <h1>My Component</h1>
+//         <p>Content in my component</p>
+//       </section>
+//     );
+//   }
+// }
+
+
+// ReactDOM.render(
+//     <MyComponent />,
+//     document.getElementById('root')
+//   );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
